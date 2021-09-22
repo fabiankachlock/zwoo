acf04d5d10ece6b406c11f3eabe332a2ac855735
fabiankachlock <fabian.kachlock@icloud.com>
2021-09-22 23:11:46 +0200

    feat(frontend): add basic styles and DarkModeSwitch

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,67 @@
 <template>
-  <router-view />
+  <div id="main">
+    <div id="header-wrapper">
+      <header>
+        <div id="row">
+          <h1 id="title">zwoo</h1>
+          <div id="menu">menu</div>
+          </div>
+      </header>
+    </div>
+    <p style="font-size: var(--f-s)">Test</p>
+    <p style="font-size: var(--f-m)">Test</p>
+    <p style="font-size: var(--f-l)">Test</p>
+    <p style="font-size: var(--f-xl)">Test</p>
+    <p style="font-size: var(--f-xxl)">Test</p>
+    <router-view />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
-<style></style>
+<style scoped>
+#main {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow-x: hidden;
+  overflow-y: scroll;
+}
+
+#header-wrapper {
+  height: 40px;
+  background-color: var(--bg-darker);
+  border-bottom: 1px solid var(--primary);
+  box-shadow: 0px 3px 5px 2px var(--shadow-dark);
+}
+
+#row {
+  height: 100%;
+  margin: 0 1rem;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+}
+
+header {
+  height: 100%;
+}
+
+#title {
+  font-size: var(--f-xxl);
+  color: var(--primary);
+}
+
+#menu {
+  height: 100%;
+}
+
+@media screen and (min-width: 768px) {
+  #header-wrapper {
+    height: 60px;
+  }
+}
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import i18n from './i18n';
+import './index.css';
 
 createApp(App).use(i18n).use(router).mount('#app');
