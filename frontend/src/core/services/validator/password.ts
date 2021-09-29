a0ca14c997be317b759043423489c2b8bba35e65
fabiankachlock <fabian.kachlock@icloud.com>
2021-09-29 22:25:47 +0200

    fix(frontend): fix password validator

--- frontend/src/core/services/validator/password.ts
+++ frontend/src/core/services/validator/password.ts
@@ -2,12 +2,14 @@ import { ValidationResult, Validator } from './_type';
 
 const NumberRegex = /[0-9]+/;
 const SpecialCharacterRegex = /[!#$%&'*+/=?^_´{|}\-[\]]+/;
+const NormalCharacterRegex = /[a-zA-Z]+/;
 
 export class PasswordValidator implements Validator<string> {
   public validate = (password: string) => {
     const containsNumber = NumberRegex.test(password);
+    const containsNormalChar = NormalCharacterRegex.test(password);
     const containsSpecialChar = SpecialCharacterRegex.test(password);
-    const longEnough = password.length > 8;
+    const longEnough = password.length >= 8;
     const errors = [] as string[];
 
     if (!longEnough) {
@@ -16,6 +18,10 @@ export class PasswordValidator implements Validator<string> {
 
     if (!containsNumber) {
       errors.push('errors.shouldContainNumber');
+    }
+
+    if (!containsNormalChar) {
+      errors.push('errors.shouldContainNormalChar');
     }
 
     if (!containsSpecialChar) {
