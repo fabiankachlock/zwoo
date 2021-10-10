4add8a0ce81ac6e6979f9639a272ae34f0326b0d
fabiankachlock <fabian.kachlock@icloud.com>
2021-10-10 20:08:59 +0200

    fix(frontend): fix quickmenu z-index

--- frontend/src/views/Menu.vue
+++ frontend/src/views/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed top-0 left-0 right-0 bg-darkest h-10 bc-primary border-b-1 shadow-sm z-50">
+  <div class="fixed top-0 left-0 right-0 bg-darkest h-10 bc-primary border-b-1 shadow-sm z-30">
     <header class="h-full">
       <div class="h-full mx-4 my-0 relative flex justify-between items-center flex-row">
         <router-link to="/home" class="cursor-pointer">
@@ -9,8 +9,10 @@
       </div>
     </header>
   </div>
-  <FloatingMenu />
-  <div class="h-screen w-screen relative flex flex-col overflow-hidden">
+  <div class="relative z-50">
+    <FloatingMenu />
+  </div>
+  <div class="h-screen w-screen relative flex flex-col overflow-hidden z-10">
     <div class="pt-10 h-full w-full">
       <div class="relative w-full h-full">
         <div class="absolute top-0 bottom-0 left-0 right-0 overflow-y-auto">
