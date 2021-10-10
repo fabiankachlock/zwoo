f7ae57db5c6bab9af2e22ec1ca248b01cf125ca8
fabiankachlock <fabian.kachlock@icloud.com>
2021-10-10 10:41:37 +0200

    feat(frontend): add floating dialog component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,7 @@
-<template>Home</template>
+<template>
+  <FloatingDialog> Test </FloatingDialog>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FloatingDialog from '@/components/misc/FloatingDialog.vue';
+</script>
