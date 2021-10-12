a2ae11f8f9c3713c46e622075884d0a4e75e8a13
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2021-10-12 22:20:00 +0200

    Got Docker Image to Run

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,15 +1,7 @@
-sudo apt update -y
-sudo apt upgrade -y
+apt-get update && apt-get install -y \
+build-essential \
+cmake \
+libboost-all-dev
 
-# install cmake
-sudo apt install cmake -y
-sudo apt install build-essential -y
-wget https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.gz
-sudo apt install git -y
-
-git clone https://github.com/fabiankachlock/zwoo.git --recursiv
-
-cd zwoo/backend/
 cmake .
 make
-
