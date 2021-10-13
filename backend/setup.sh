6ca1044ebec756339a305878eed1993b107ff732
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2021-10-13 20:51:01 +0200

    updated docker img

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,7 +1,3 @@
-apt-get update && apt-get install -y \
-build-essential \
-cmake \
-libboost-all-dev
+#!/bin/bash
 
-cmake .
-make
+apt-get update && apt-get install -y \ build-essential \ cmake \ libboost-all-dev
