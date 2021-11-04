58ba83e1910f14b504d8cdd8c97e8a79b1ee05a6
timo3400 <timolus1@gmail.com>
2021-11-04 20:44:04 +0100

    Database stuff

--- backend/include/Database/DatabaseHandler.h
+++ backend/include/Database/DatabaseHandler.h
@@ -4,32 +4,13 @@
 #include <string>
 #include <iostream>
 
-#include "bsoncxx/builder/stream/document.hpp"
-#include "bsoncxx/json.hpp"
-#include "bsoncxx/oid.hpp"
-#include "mongocxx/client.hpp"
-#include "mongocxx/database.hpp"
-#include "mongocxx/uri.hpp"
 
 namespace Backend::Database
 {
     class DatabaseHandler
     {
     public:
-        DatabaseHandler(std::string host, int port, std::string db_name, std::string user, std::string password);
-        ~DatabaseHandler();
-
-        std::string GetId(std::string field_name, std::String value);
-
-        bsoncxx::document::element GetElement(std::string &id, const char* field_name);
-        bool SetElement(std::string &id, std::string field_name, std::string data);
-
-        bool DeleteRow(std::string &id);
-
     private:
-        const char *m_dbUri;
-        const char *m_dbName;
-        const char *m_dbCollectionName;
     };
 }
 #endif _DATABASE_HANDLER_H_
--- backend/setup.sh
+++ backend/setup.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 apt-get update && DEBIAN_FRONTEND="noninteractive" TZ="Germany/Berlin" apt-get install -y tzdata
-apt-get install -y build-essential cmake libboost-all-dev protobuf-compiler libssl-dev libcurl4-openssl-dev libmongoc-1.0-0 libbson-1.0 libsasl2-dev zlib1g-dev
-
-# Compile mongocxx
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.17.0/mongo-c-driver-1.17.0.tar.gz
-tar xzf mongo-c-driver-1.17.0.tar.gz
-cd mongo-c-driver-1.17.0
-mkdir cmake-build
-cd cmake-build
-cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
-sudo make install
-
-git clone https://github.com/mongodb/mongo-cxx-driver.git --branch releases/stable --depth 1
-cd mongo-cxx-driver/build
-
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release -DBSONCXX_POLY_USE_MNMLSTC=1 -DCMAKE_INSTALL_PREFIX=/usr/local
-
-sudo make
-sudo make install
-cd $TOP_DIR
-rm -rf mongo-c-driver-1.17.0*
+apt-get install -y build-essential cmake libboost-all-dev protobuf-compiler libssl-dev libcurl4-openssl-dev libsasl2-dev zlib1g-dev
+ 
+wget https://dev.mysql.com/get/Downloads/Connector-C++/libmysqlcppconn9_8.0.27-1ubuntu20.04_amd64.deb
+dpkg --install ./libmysqlcppconn9_8.0.27-1ubuntu20.04_amd64.deb
+rm ./libmysqlcppconn9_8.0.27-1ubuntu20.04_amd64.deb
 
 #cd backend
 cmake . && make
