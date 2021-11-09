9ce0505f8a6f77b07c36aa7f67efc7740fe75295
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2021-11-09 19:09:28 +0100

    added sqlite as CMake subproject

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "../lib/sqlite/sqlite3.h"
+#include "sqlite/sqlite3.h"
 
 int main()
 {
     sqlite3* con = NULL;
-    sqlite3_open("../test.db", &con);
+    con = (sqlite3*)sqlite3_open("./test.db", &con);
 
     return 0;
 }
