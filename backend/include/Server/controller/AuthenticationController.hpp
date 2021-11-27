5a7a606598e84e7bcb2e88663eccc765f9bb5fb7
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2021-11-27 17:41:27 +0100

    reCaptcha Backend validation #18

--- backend/include/Server/controller/AuthenticationController.hpp
+++ backend/include/Server/controller/AuthenticationController.hpp
@@ -1,12 +1,18 @@
 #ifndef _AUTHENTICATION_CONTROLLER_HPP_
 #define _AUTHENTICATION_CONTROLLER_HPP_
 
+#include <stdio.h>
+#include <sstream>
+
+#include <curl/curl.h>
+
 #include "oatpp/web/server/api/ApiController.hpp"
 #include "oatpp/parser/json/mapping/ObjectMapper.hpp"
 #include "oatpp/core/macro/codegen.hpp"
-
 #include "oatpp/web/protocol/http/Http.hpp"
 #include "oatpp/core/macro/component.hpp"
+
+#include "Server/dto/AuthenticationDTO.hpp"
 
 #include OATPP_CODEGEN_BEGIN(ApiController) // <- Begin Codegen
 
@@ -19,6 +25,12 @@ public:
 
 public:
 
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+    {
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
+        return size * nmemb;
+    }
+
     static std::shared_ptr<AuthenticationController> createShared(
             OATPP_COMPONENT(std::shared_ptr<ObjectMapper>, objectMapper) // Inject objectMapper component here as default parameter
     ){
@@ -26,14 +38,53 @@ public:
     }
 
     ENDPOINT("GET", "/hello-world", helloWorld) {
-        auto response = createResponse(Status::CODE_200, "");
+        auto response = createResponse(Status::CODE_200, R"({"message": "Hello Wolrd!"})");
         return response;
     }
-
     ENDPOINT_INFO(helloWorld) {
         info->summary = "Hello World Testendpoint.";
 
         info->addResponse<Object<StatusDto>>(Status::CODE_200, "application/json");
+        info->addResponse<Object<StatusDto>>(Status::CODE_404, "application/json");
+        info->addResponse<Object<StatusDto>>(Status::CODE_500, "application/json");
+    }
+
+    ENDPOINT("POST", "auth/recaptcha", reCaptcha, BODY_STRING(String, token)) {
+        auto dto = reCaptchaDto::createShared();
+        std::string readBuffer;
+        // request to google servers
+        {
+            CURL *curl;
+            CURLcode res;
+
+
+            curl = curl_easy_init();
+            if (curl)
+            {
+                curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+                // TODO: hmmmm
+                std::stringstream str;
+                str << "secret=" << SITESECRET << "&response=" << token.getValue("token");
+                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, str.get());
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+                res = curl_easy_perform(curl);
+                if(res != CURLE_OK)
+                    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                            curl_easy_strerror(res));
+
+                curl_easy_cleanup(curl);
+            }
+            curl_global_cleanup();
+        }
+
+        auto res = createResponse(Status::CODE_200, readBuffer);
+        return res;
+    }
+    ENDPOINT_INFO(reCaptcha) {
+        info->description = "verify the reCaptcha with this Endpoint.";
+
+        info->addResponse<Object<reCaptchaDto>>(Status::CODE_200, "application/json");
         info->addResponse<Object<StatusDto>>(Status::CODE_404, "application/json");
         info->addResponse<Object<StatusDto>>(Status::CODE_500, "application/json");
     }
