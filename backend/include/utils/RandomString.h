e1fd6ea25512ec4b1846f63af52432d44110cbfe
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2021-12-28 17:25:09 +0100

    Update RandomString.h

--- backend/include/utils/RandomString.h
+++ backend/include/utils/RandomString.h
@@ -4,9 +4,14 @@
 #include <ctime>
 #include <iostream>
 #include <unistd.h>
+#include <stdlib.h>
+#include <chrono>
 #include <random>
 
 namespace Backend {
+
+    int randomNumberInRange(int min, int max);
+
     std::string randomString(const int len) 
     {
         static const char alphanum[] =
@@ -16,8 +21,10 @@ namespace Backend {
         std::string tmp_s;
         tmp_s.reserve(len);
 
+        std::mt19937 rng();
+
         for (int i = 0; i < len; ++i) {
-            tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+            tmp_s += alphanum[randomNumberInRange(0, 512) % (sizeof(alphanum) - 1)];
         }
 
         return tmp_s;
@@ -25,8 +32,7 @@ namespace Backend {
 
     int randomNumberInRange(int min, int max)
     {
-        std::random_device rd;
-        std::mt19937 gen(rd());
+        std::mt19937 gen(std::chrono::steady_clock::now().time_since_epoch().count());
         std::uniform_int_distribution<> dis(min, max);
         return dis(gen);
     }
