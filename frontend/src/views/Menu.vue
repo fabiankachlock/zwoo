1436742924a1f6af0c2409c895d6d69bc6c3c4bd
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-02 19:19:52 +0100

    feat: branding (logo)

--- frontend/src/views/Menu.vue
+++ frontend/src/views/Menu.vue
@@ -2,7 +2,8 @@
   <div class="fixed top-0 left-0 right-0 bg-darkest h-10 bc-primary border-b-1 shadow-sm z-30">
     <header class="h-full">
       <div class="h-full mx-4 my-0 relative flex justify-between items-center flex-row">
-        <router-link to="/home" class="cursor-pointer">
+        <router-link to="/home" class="cursor-pointer flex flex-row items-center justify-start">
+          <img src="/img/logo/zwoo_logo_simple_none.svg" class="max-h-8 mr-4" />
           <h1 class="tc-primary text-4xl">zwoo</h1>
         </router-link>
         <NavBar />
