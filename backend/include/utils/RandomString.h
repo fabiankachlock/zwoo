36e456b2c52687c049b46269d0e0c5121e3ae747
fabiankachlock <fabian.kachlock@icloud.com>
2022-01-07 21:50:45 +0100

    fix: backend compilation

--- backend/include/utils/RandomString.h
+++ backend/include/utils/RandomString.h
@@ -12,45 +12,10 @@ namespace Backend {
 
     int randomNumberInRange(int min, int max);
 
-    std::string randomString(const int len) 
-    {
-        static const char alphanum[] =
-                "0123456789"
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                "abcdefghijklmnopqrstuvwxyz";
-        std::string tmp_s;
-        tmp_s.reserve(len);
+    std::string randomString(const int len);
 
-        std::mt19937 rng();
+    std::string randomNDigitNumber(int n);
 
-        for (int i = 0; i < len; ++i) {
-            tmp_s += alphanum[randomNumberInRange(0, 512) % (sizeof(alphanum) - 1)];
-        }
-
-        return tmp_s;
-    }
-
-    int randomNumberInRange(int min, int max)
-    {
-        std::mt19937 gen(std::chrono::steady_clock::now().time_since_epoch().count());
-        std::uniform_int_distribution<> dis(min, max);
-        return dis(gen);
-    }
-
-    std::string randomNDigitNumber(int n)
-    {
-        std::string out = "";
-        out.reserve(n);
-
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> dis(0, 9);
-
-        for (int i = 0; i < n; ++i)
-            out += (char)(dis(gen) + 48);
-
-        return out;
-    }
 }// namespace Backend
 
 
--- backend/include/utils/Validator.h
+++ backend/include/utils/Validator.h
@@ -5,22 +5,11 @@
 
 #include <regex>
 
-bool isValidEmail(const char* email)
-{
-    const std::regex reg(EMAIL_REGEX);
-    std::cmatch m;
-    return std::regex_match (email, m, reg);
-}
+bool isValidEmail(const char* email);
 
-bool isValidPassword(const char* pw)
-{
-    std::cmatch m;
-    const std::regex r1(NUMBERS_REGEX);
-    const std::regex r2(SPECIAL_CHARACTERS_REGEX);
-    const std::regex r3(NORMAL_CHARACTER_REGEX);
 
-    return std::regex_search(pw, m, r1) && std::regex_search(pw, m, r2) && std::regex_search(pw, m, r3);
-}
+bool isValidPassword(const char* pw);
+
 
 
 #endif // _VALIDATOR_H_
