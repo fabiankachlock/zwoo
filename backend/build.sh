caf02b0455fe003bdf6da10dec6967ea27d62c03
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2022-01-20 15:33:30 +0100

    added Docker Image Build

---
+++ backend/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo username: $1
+echo password: $2
+
+apt update
+apt update && DEBIAN_FRONTEND="noninteractive" TZ="Germany/Berlin" apt-get install -y tzdata
+apt install -y build-essential cmake python3 python3-pip
+pip install conan
+
+conan remote add conan-remote https://artifrog.igd20.de/artifactory/api/conan/conan-igd20
+conan user -p $2 -r conan-remote $1
+
+mkdir build && cd build
+conan install .. --build
+cmake ..
+
+make -j$(nproc)
