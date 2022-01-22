90a2ef4d63bb35673fbc37145befdd93bfbb25fa
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2022-01-22 23:05:10 +0100

    Fixed Dockerimage Build
    Fixed Linker Error: undefined reference
    -> conan install from libstdc++ to libstdc++11

--- backend/build.sh
+++ backend/build.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-echo username: $1
-echo password: $2
-
 apt update
 apt update && DEBIAN_FRONTEND="noninteractive" TZ="Germany/Berlin" apt-get install -y tzdata
-apt install -y build-essential cmake python3 python3-pip
+apt install -y cmake python3 python3-pip git
 pip install conan
 
 conan remote add conan-remote $3
 conan user -p $2 -r conan-remote $1
 
 mkdir build && cd build
-conan install .. --build
+conan install .. -s compiler.libcxx=libstdc++11 --build
 cmake ..
 
 make -j$(nproc)
