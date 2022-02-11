7584d24c891577a51c7f98e54c1526bfe5aadef7
fabiankachlock <fabian.kachlock@icloud.com>
2022-02-11 16:18:20 +0100

    feat: cookie dialog translations

--- frontend/src/components/cookies/CookieDialog.vue
+++ frontend/src/components/cookies/CookieDialog.vue
@@ -1,34 +1,34 @@
 <template>
   <FloatingDialog>
     <div class="flex flex-col mx-2 overflow-y-scroll">
-      <h2 class="tc-main text-xl text-center my-2">Consent to all those tasty cookies</h2>
+      <h2 class="tc-main text-xl text-center my-2">{{ t('cookies.title') }}</h2>
       <div class="bc-darkest border-t border-b py-1">
-        <h3 class="tc-main text-lg">Functional cookies</h3>
+        <h3 class="tc-main text-lg">{{ t('cookies.functional.title') }}</h3>
         <ul class="ml-4 list">
           <li class="bc-main">
             <div>
-              <h4 class="text tc-main">Authentication Cookies</h4>
-              <p class="text-xs tc-main-secondary ml-2 mb-1">Use for session and login</p>
+              <h4 class="text tc-main">{{ t('cookies.functional.auth.name') }}</h4>
+              <p class="text-xs tc-main-secondary ml-2 mb-1">{{ t('cookies.functional.auth.description') }}</p>
             </div>
-            <div class="bg-main border-2 border-transparent px-4 py-1 rounded cursor-not-allowed select-none">
+            <div class="bg-main border-2 border-transparent px-4 py-1 ml-4 rounded cursor-not-allowed select-none">
               <p class="tc-main-secondary flex flex-row flex-nowrap justify-end items-center">
-                <span> Active </span>
+                <span>{{ t('cookies.active') }}</span>
                 <Icon icon="akar-icons:check" class="ml-2" />
               </p>
             </div>
           </li>
           <li class="bc-main">
             <div>
-              <h4 class="text tc-main">Google Recaptcha Cookies</h4>
-              <p class="text-xs tc-main-secondary ml-2 mb-1">Google reCAPTCHA Service</p>
+              <h4 class="text tc-main">{{ t('cookies.functional.recaptcha.name') }}</h4>
+              <p class="text-xs tc-main-secondary ml-2 mb-1">{{ t('cookies.functional.recaptcha.description') }}</p>
             </div>
             <div
-              class="bg-main border-2 border-transparent px-4 py-1 rounded transition hover:bg-dark cursor-pointer select-none"
+              class="bg-main border-2 border-transparent px-4 py-1 ml-4 rounded transition hover:bg-dark cursor-pointer select-none"
               :class="{ 'tc-primary': recaptchaActivated, 'tc-main-secondary': !recaptchaActivated }"
               @click="recaptchaActivated = !recaptchaActivated"
             >
               <p class="flex flex-row flex-nowrap justify-end items-center">
-                <span> Active </span>
+                <span> {{ t(recaptchaActivated ? 'cookies.active' : 'cookies.inactive') }} </span>
                 <Icon v-if="recaptchaActivated" icon="akar-icons:check" class="ml-2" />
                 <Icon v-else icon="akar-icons:cross" class="ml-2" />
               </p>
@@ -37,11 +37,15 @@
         </ul>
       </div>
       <div class="grid grid-cols-2 gap-2 mt-4">
-        <div class="bg-light border-2 border-transparent px-4 py-1 rounded transition hover:bg-main cursor-pointer select-none">
-          <p class="tc-main-secondary text-center">Accept Selection</p>
+        <div
+          class="flex justify-center items-center bg-light border-2 border-transparent px-4 py-1 rounded transition hover:bg-main cursor-pointer select-none"
+        >
+          <p class="tc-main-secondary text-center">{{ t('cookies.acceptSelection') }}</p>
         </div>
-        <div class="bg-main border-2 border-primary px-4 py-1 rounded transition hover:bg-dark cursor-pointer select-none">
-          <p class="tc-primary text-center">Accept All</p>
+        <div
+          class="flex justify-center items-center bg-main border-2 border-primary px-4 py-1 rounded transition hover:bg-dark cursor-pointer select-none"
+        >
+          <p class="tc-primary text-center">{{ t('cookies.acceptAll') }}</p>
         </div>
       </div>
     </div>
@@ -52,6 +56,9 @@
 import FloatingDialog from '../misc/FloatingDialog.vue';
 import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const recaptchaActivated = ref(true);
 </script>
