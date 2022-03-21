41c0cd8d9d3a140c740c5c26bf5b3faeeed0c8bd
fabiankachlock <fabian.kachlock@icloud.com>
2022-03-21 20:44:43 +0100

    feat: finalize chat

--- frontend/src/components/game/chat/ChatMessage.vue
+++ frontend/src/components/game/chat/ChatMessage.vue
@@ -1,15 +1,17 @@
 <template>
-  <div :class="{ 'justify-end': isOwn }" class="flex justify-start w-full my-0.5 max-w-full">
+  <div :class="{ 'justify-end': isOwn }" class="flex justify-start w-full my-0.5 max-w-full relative">
     <p
       :class="{
-        'rounded-bl-none border-transparent': !isOwn,
+        'rounded-bl-none border-transparent mb-3': !isOwn,
         'rounded-br-none bc-primary': isOwn,
-        'bg-dark tc-main-secondary': isSpectator
+        'bg-main tc-main-light': isSpectator,
+        'bg-dark tc-main-secondary': !isSpectator
       }"
-      class="bg-main tc-main-secondary px-2 py-0.5 rounded-2xl border break-all"
+      class="px-2 py-0.5 rounded-2xl border break-all"
     >
       {{ message }}
     </p>
+    <p v-if="!isOwn" class="text-xs absolute -bottom-0.5 italic">{{ name }}</p>
   </div>
 </template>
 
@@ -20,7 +22,8 @@ const props = defineProps<{
   message: string;
   isOwn?: boolean;
   isSpectator?: boolean;
+  name: string;
 }>();
 
-const { message, isOwn } = toRefs(props);
+const { message, isOwn, name } = toRefs(props);
 </script>
--- frontend/src/components/game/chat/GameChat.vue
+++ frontend/src/components/game/chat/GameChat.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="h-32 py-2 px-3 flex flex-col flex-nowrap items-center overflow-y-auto" style="max-height: 60vh">
-    <ChatMessage v-for="message in messages" :message="message.message" :key="message.id" :is-own="message.sender.id === auth.username" />
+    <ChatMessage
+      v-for="message in messages"
+      :message="message.message"
+      :key="message.id"
+      :is-own="message.sender.id === auth.username"
+      :is-spectator="message.sender.role === ZRPRole.Spectator"
+      :name="message.sender.id"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { useAuth } from '@/core/adapter/auth';
 import { useChatStore } from '@/core/adapter/play/chat';
+import { ZRPRole } from '@/core/services/zrp/zrpTypes';
 import { computed } from 'vue';
 import ChatMessage from './ChatMessage.vue';
 
