eaacc0dbf40361ad09eabfd78be85728c1e2d5d8
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2022-03-24 19:13:34 +0100

    moved thread code to thread.cpp

--- backend/include/utils/thread.h
+++ backend/include/utils/thread.h
@@ -6,50 +6,7 @@
 #include <thread>
 #include <functional>
 
-void periodicFunction(std::function<void(void)> func, unsigned int interval)
-{
-    std::thread([func, interval]()
-    {
-        while (true)
-        {
-            auto t = std::chrono::steady_clock::now() + std::chrono::milliseconds(interval);
-            func();
-            std::this_thread::sleep_until(t);
-        }
-    }).detach();
-}
-
-void timedFunction(std::function<void(void)> func, uint8_t hour, uint8_t minute)
-{
-    int t = (minute + (hour * 60)) * 60;
-    std::thread([func, t]()
-    {
-        while (true)
-        {
-            time_t theTime = time(NULL);
-            struct tm *aTime = localtime(&theTime);
-
-            int ct = aTime->tm_sec + ((aTime->tm_min + (aTime->tm_hour * 60)) * 60);
-            int rt;
-
-            if (ct < t)
-            {
-                rt = t - ct;
-            }
-            else if (ct > t)
-            {
-                rt = 86400 - ct + t;
-            }
-            else
-            {
-                rt = 86400;
-            }
-
-            std::this_thread::sleep_for(std::chrono::seconds(rt));
-
-            func();
-        }
-    }).detach();
-}
+void periodicFunction(std::function<void(void)> func, unsigned int interval);
+void timedFunction(std::function<void(void)> func, uint8_t hour, uint8_t minute);
 
 #endif // _THREAD_H_
