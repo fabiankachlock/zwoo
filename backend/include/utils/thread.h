53f82e61ec8d4b4f72ad4042cccfd2c1f286a392
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2022-03-24 17:19:43 +0100

    added Timed  function caller

--- backend/include/utils/thread.h
+++ backend/include/utils/thread.h
@@ -2,6 +2,7 @@
 #define _THREAD_H_
 
 #include <chrono>
+#include <time.h>
 #include <thread>
 #include <functional>
 
@@ -18,15 +19,35 @@ void periodicFunction(std::function<void(void)> func, unsigned int interval)
     }).detach();
 }
 
-void TimedFunction(std::function<void(void)> func, unsigned int interval)
+void TimedFunction(std::function<void(void)> func, uint8_t hour, uint8_t minute)
 {
-    std::thread([func, interval]()
+    int t = (minute + (hour * 60)) * 60;
+    std::thread([func, t]()
     {
         while (true)
         {
-            auto t = std::chrono::steady_clock::now() + std::chrono::milliseconds(interval);
+            time_t theTime = time(NULL);
+            struct tm *aTime = localtime(&theTime);
+
+            int ct = aTime->tm_sec + ((aTime->tm_min + (aTime->tm_hour * 60)) * 60);
+            int rt;
+
+            if (ct < t)
+            {
+                rt = t - ct;
+            }
+            else if (ct > t)
+            {
+                rt = 86400 - ct + t;
+            }
+            else
+            {
+                rt = 86400;
+            }
+
+            std::this_thread::sleep_for(std::chrono::seconds(rt));
+
             func();
-            std::this_thread::sleep_until(t);
         }
     }).detach();
 }
