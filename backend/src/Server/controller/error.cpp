ce4df0d2f886ec308c043112d8d05ad78dc9c328
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2022-03-27 13:30:32 +0200

    now everything should work

--- backend/src/Server/controller/error.cpp
+++ backend/src/Server/controller/error.cpp
@@ -1,6 +1,6 @@
 #include "Server/controller/error.h"
 
-std::string constructZwooErrorMessage(std::string message, e_Errors code)
+std::string constructErrorMessage(std::string message, e_Errors code)
 {
     return "{\"message\": " + message + ", \"code\": " + std::to_string((int)code) + "}";
 }
