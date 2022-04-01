b3c0375eb606254aaa38fb1b96d408bbbf0f5dde
Visual-Rock <48381849+Visual-Rock@users.noreply.github.com>
2022-04-01 12:34:53 +0200

    Code Sharing in ErrorHandler

--- backend/src/Server/ErrorHandler.cpp
+++ backend/src/Server/ErrorHandler.cpp
@@ -11,41 +11,30 @@ ErrorHandler::ErrorHandler(const std::shared_ptr<oatpp::data::mapping::ObjectMap
 
 std::shared_ptr<ErrorHandler::OutgoingResponse>
 ErrorHandler::handleError(const Status& status, const oatpp::String& message, const Headers& headers) {
+    
+    OutgoingResponse response;
 
     if ( status.code < 500 )
-    {
-        auto response = ResponseFactory::createResponse(status, message, m_objectMapper);
-
-        for(const auto& pair : headers.getAll()) {
-            response->putHeader(pair.first.toString(), pair.second.toString());
-        }
-
-        response->putHeader("Access-Control-Allow-Origin", ZWOO_CORS);
-        response->putHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-        response->putHeader("Access-Control-Allow-Credentials", "true");
-
-        return response;
-    }
+        response = ResponseFactory::createResponse(status, message, m_objectMapper);
     else
     {
+        auto logger = spdlog::get("BED");
+        logger->error("Backend error: {}", message->c_str());
+
         auto error = StatusDto::createShared();
         error->status = "ERROR";
         error->code = status.code;
         error->message = constructErrorMessage("Backend Error", e_Errors::BACKEND_ERROR);
 
-        auto response = ResponseFactory::createResponse(status, error, m_objectMapper);
-
-        auto logger = spdlog::get("BED");
-        logger->error("Backend error: {}", message->c_str());
-
-        for(const auto& pair : headers.getAll()) {
-            response->putHeader(pair.first.toString(), pair.second.toString());
-        }
-
-        response->putHeader("Access-Control-Allow-Origin", ZWOO_CORS);
-        response->putHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-        response->putHeader("Access-Control-Allow-Credentials", "true");
-
-        return response;
+        response = ResponseFactory::createResponse(status, error, m_objectMapper);
     }
+
+    for(const auto& pair : headers.getAll()) 
+        response->putHeader(pair.first.toString(), pair.second.toString());
+
+    response->putHeader("Access-Control-Allow-Origin", ZWOO_CORS);
+    response->putHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    response->putHeader("Access-Control-Allow-Credentials", "true");
+
+    return response;
 }
