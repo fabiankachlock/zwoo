20b908f27ffe45226a37e751547156e81317aa0f
fabiankachlock <fabian.kachlock@icloud.com>
2022-04-01 16:31:25 +0200

    feat: add game summary ui

---
+++ frontend/src/core/adapter/play/summary.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia';
+
+export const useGameSummary = defineStore('game-summary', {
+  state: () => ({
+    positions: {} as Record<string, number>
+  }),
+  actions: {
+    async requestGameSummary(): Promise<{ name: string; position: number }[]> {
+      this._computePositions();
+      return Object.entries(this.positions)
+        .map(([name, position]) => ({
+          name,
+          position
+        }))
+        .sort((a, b) => a.position - b.position);
+    },
+    _computePositions() {
+      this.positions['test-winner'] = 1;
+      this.positions['test-second'] = 2;
+      this.positions['test-third'] = 3;
+      this.positions['test-third-2'] = 3;
+    }
+  }
+});
--- frontend/src/views/game/Summary.vue
+++ frontend/src/views/game/Summary.vue
@@ -1,3 +1,48 @@
-<template>Summary</template>
+<template>
+  <div class="game-summary overflow-y-scroll">
+    <template v-if="players.length > 1">
+      <div class="sticky top-0 m-1 bg-dark hover:bg-darkest rounded-lg px-3 py-2 border bc-primary">
+        <p class="tc-main text-xl">{{ players[0].name }} - Winner!</p>
+      </div>
+      <div v-for="player in notWinners" :key="player.name" class="player m-1 bg-lightest hover:bg-light rounded px-3 py-2 border bc-dark">
+        <p class="tc-main-dark">{{ player.position }}. {{ player.name }}</p>
+      </div>
+      <div class="bottom-spacer h-8"></div>
+    </template>
+    <template v-else> fetching results </template>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useGameSummary } from '@/core/adapter/play/summary';
+import { computed } from '@vue/reactivity';
+import { onMounted, ref } from 'vue';
+
+const summary = useGameSummary();
+const players = ref<{ name: string; position: number }[]>([]);
+const notWinners = computed(() => [...players.value].slice(1) ?? []);
+
+onMounted(async () => {
+  players.value = await summary.requestGameSummary();
+});
+</script>
+
+<style scoped>
+.game-summary {
+  position: absolute;
+  top: 10%;
+  left: 3rem;
+  right: 3rem;
+  bottom: 0;
+}
+
+.player {
+  @apply mx-3;
+}
+
+@media only screen and (min-width: 672px) {
+  .player {
+    @apply mx-8;
+  }
+}
+</style>
