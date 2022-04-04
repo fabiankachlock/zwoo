4616077b6dd7a7204aaf6b45b7691f4e3616ac7b
Visual-Rock <klon135christoph@gmail.com>
2022-04-04 18:35:40 +0200

    Added All 1xx ZRP Codes

--- backend/include/Server/controller/GameManager/websocket/ZRPCodes.h
+++ backend/include/Server/controller/GameManager/websocket/ZRPCodes.h
@@ -8,6 +8,7 @@ enum e_Roles {
 };
 
 enum e_ZRPOpCodes {
+    // General
     PLAYER_JOINED = 100,
     SPECTATOR_JOINED = 101,
     PLAYER_LEFT = 102,
@@ -16,7 +17,18 @@ enum e_ZRPOpCodes {
     RECEIVE_MESSAGE = 105,
     PLAYER_LEAVE = 106,
     GET_ALL_PLAYERS_IN_LOBBY = 108,
-    ALL_PLAYERS_IN_LOBBY = 109
+    ALL_PLAYERS_IN_LOBBY = 109,
+    SPECTATOR_TO_PLAYER = 110,
+    PLAYER_TO_SPECTATOR = 111,
+    PLAYER_TO_HOST = 112,
+    YOU_ARE_HOST_NOW = 113,
+    NEW_HOST = 114,
+    KICK_PLAYER = 115,
+    PLAYER_CHANGED_ROLE = 116
+
+    // Lobby
+
+    // Game
 };
 
 #endif
