e074594d5b2a6de0d0f637aef2849d552edf89cc
fabiankachlock <fabian.kachlock@icloud.com>
2022-04-17 21:51:03 +0200

    feat: show extra information to themes and if there active

--- frontend/src/components/themes/ThemesGalleryEntry.vue
+++ frontend/src/components/themes/ThemesGalleryEntry.vue
@@ -2,7 +2,19 @@
   <div class="flex flex-col flex-nowrap bg-dark rounded-lg w-full border-2 bc-darkest hover:bc-primary py-4 relative">
     <h3 class="tc-main-dark text-2xl mx-4 mb-2">
       {{ props.theme.name }}
+      <Icon v-if="props.isSelected" icon="fluent:checkmark-circle-32-regular" class="inline-block tc-secondary ml-2" />
     </h3>
+    <div class="mx-4 my-2">
+      <ul class="tags flex flex-row justify-start items-center tc-main text-sm">
+        <li
+          v-if="props.theme.isMultilayer || true"
+          class="inline-tag bg-light hover:bg-main bc-light hover:bc-darkest flex flex-row flex-nowrap items-center"
+        >
+          <Icon icon="fluent:layer-20-filled" class="mr-2 tc-primary text-xl" />
+          MultiLayer
+        </li>
+      </ul>
+    </div>
     <div class="mx-4">
       <p class="tc-main-light italic" v-if="props.theme.description">
         {{ props.theme.description }}
@@ -18,7 +30,7 @@
         <span
           v-for="variant in props.theme.variants"
           :key="variant"
-          class="tc-main text-base mx-1 py-1 px-2 rounded bg-light hover:bg-main border bc-light hover:bc-darkest cursor-pointer"
+          class="tc-main inline-tag bg-light hover:bg-main bc-light hover:bc-darkest cursor-pointer"
         >
           {{ variant }}
         </span>
@@ -33,9 +45,17 @@
 
 <script setup lang="ts">
 import { defineProps } from 'vue';
+import { Icon } from '@iconify/vue';
 import { CardThemeInformation } from '@/core/services/cards/CardThemeConfig';
 
 const props = defineProps<{
   theme: CardThemeInformation;
+  isSelected: boolean;
 }>();
 </script>
+
+<style>
+.inline-tag {
+  @apply text-base mr-2 py-1 px-2 rounded  border;
+}
+</style>
--- frontend/src/views/themes/Gallery.vue
+++ frontend/src/views/themes/Gallery.vue
@@ -1,7 +1,7 @@
 <template>
   <h2 class="text-4xl tc-main text-center my-6">zwoo - Theme&nbsp;Gallery</h2>
   <div class="flex flex-col mx-2 sm:mx-8 my-2">
-    <ThemesGalleryEntry v-for="theme in themes" :key="theme.name" :theme="theme" />
+    <ThemesGalleryEntry v-for="theme in themes" :key="theme.name" :theme="theme" :is-selected="selectedCardTheme === theme.name" />
   </div>
 </template>
 
@@ -10,8 +10,12 @@ import { CardThemeInformation } from '@/core/services/cards/CardThemeConfig';
 import { CardThemeManager } from '@/core/services/cards/ThemeManager';
 import { onMounted, ref } from 'vue';
 import ThemesGalleryEntry from '@/components/themes/ThemesGalleryEntry.vue';
+import { useConfig } from '@/core/adapter/config';
+import { computed } from '@vue/reactivity';
 
 const themes = ref<CardThemeInformation[]>([]);
+const config = useConfig();
+const selectedCardTheme = computed(() => config.cardTheme);
 
 onMounted(async () => {
   const loadedThemes = await CardThemeManager.global.getAllThemesInformation();
