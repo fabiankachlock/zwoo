63a206aab064c191490b8de11e389c00154c9a1b
fabiankachlock <fabian.kachlock@icloud.com>
2022-04-18 22:02:53 +0200

    feat: improve them gallery layout

--- frontend/src/components/themes/ThemesGalleryEntry.vue
+++ frontend/src/components/themes/ThemesGalleryEntry.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="flex flex-col flex-nowrap bg-dark rounded-lg w-full border-2 bc-darkest hover:bc-primary py-4 relative">
-    <h3 class="tc-main-dark text-2xl mx-4 mb-2">
-      {{ props.theme.name }}
-      <Icon v-if="props.isSelected" icon="fluent:checkmark-circle-32-regular" class="inline-block tc-secondary ml-2" />
-    </h3>
-    <div class="mx-4 my-2">
-      <ul class="tags flex flex-row justify-start items-center tc-main text-sm">
-        <li
-          v-if="props.theme.isMultilayer || true"
-          class="inline-tag bg-light hover:bg-main bc-light hover:bc-darkest flex flex-row flex-nowrap items-center"
-        >
-          <Icon icon="fluent:layer-20-filled" class="mr-2 tc-primary text-xl" />
-          MultiLayer
-        </li>
-      </ul>
+    <div class="flex flex-row flex-wrap justify-between items-center mb-2">
+      <h3 class="tc-main-dark text-2xl mx-4">
+        {{ props.theme.name }}
+        <Icon v-if="props.isSelected" icon="fluent:checkmark-circle-32-regular" class="inline-block tc-secondary ml-2" />
+      </h3>
+      <div class="mx-4">
+        <ul class="tags flex flex-row justify-start items-center tc-main text-sm">
+          <li
+            v-if="props.theme.isMultilayer || true"
+            class="inline-tag bg-light hover:bg-main bc-light hover:bc-darkest flex flex-row flex-nowrap items-center"
+          >
+            <Icon icon="fluent:layer-20-filled" class="mr-2 tc-primary text-xl" />
+            MultiLayer
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="mx-4">
       <p class="tc-main-light italic" v-if="props.theme.description">
@@ -43,7 +45,7 @@
     <div class="divider bc-darkest h-0 my-2 border-2 border-solid border-t-0"></div>
     <div class="mx-4">
       <div class="flex flex-row flex-nowrap justify-end items-center mt-2">
-        <button class="py-2 px-3 rounded ml-2 bg-light hover:bg-main text-lg tc-primary" @click="selectAsTheme()">Select As Theme</button>
+        <button class="py-2 px-3 rounded ml-2 bg-light hover:bg-main tc-primary" @click="selectAsTheme()">Select As Theme</button>
       </div>
     </div>
   </div>
