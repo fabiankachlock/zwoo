3dcde5c557044cb851ae4a388baf854d373e02f5
fabiankachlock <fabian.kachlock@icloud.com>
2022-05-19 21:46:53 +0200

    feat: add in game modal response system

--- frontend/src/components/game/modals/ColorSelectModal.vue
+++ frontend/src/components/game/modals/ColorSelectModal.vue
@@ -1,3 +1,16 @@
-<template>Color Modal</template>
+<template>Color Modal <button @click="close">Close</button></template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useGameModal } from '@/core/adapter/play/modal';
+import { CardColor } from '@/core/services/game/card';
+import { ref } from 'vue';
+
+const modalState = useGameModal();
+const selectedColor = ref<CardColor | undefined>(undefined);
+
+const close = () => {
+  if (selectedColor.value) {
+    modalState.closeSelf(selectedColor.value);
+  }
+};
+</script>
--- frontend/src/core/adapter/play/modal.ts
+++ frontend/src/core/adapter/play/modal.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import { Component } from 'vue';
 import ColorSelectModal from '@/components/game/modals/ColorSelectModal.vue';
+import { CardColor } from '@/core/services/game/card';
 
 export enum InGameModal {
   ColorPicker = 'ColorPicker'
@@ -10,9 +11,14 @@ const gameModals: Record<InGameModal, Component> = {
   [InGameModal.ColorPicker]: ColorSelectModal
 };
 
+export type InGameModalResponse = {
+  [InGameModal.ColorPicker]: CardColor;
+};
+
 export const useGameModal = defineStore('game-modal', {
   state: () => ({
     currentModal: undefined as string | undefined,
+    _modalResponse: undefined as undefined | InGameModalResponse[keyof InGameModalResponse],
     modalComponent: undefined as Component | undefined
   }),
 
@@ -21,9 +27,10 @@ export const useGameModal = defineStore('game-modal', {
       this.currentModal = type;
       this.modalComponent = gameModals[type];
     },
-    closeModal(): void {
+    closeSelf(response: InGameModalResponse[keyof InGameModalResponse]): void {
       this.currentModal = undefined;
       this.modalComponent = undefined;
+      this._modalResponse = response;
     }
   }
 });
