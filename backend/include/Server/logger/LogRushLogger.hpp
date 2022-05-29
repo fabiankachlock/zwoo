e28c119dc0cd9ab2ef55a9474530f254fc9d9ebe
Visual-Rock <visualrock.5329@gmail.com>
2022-05-29 15:44:41 +0200

    formatting

--- backend/include/Server/logger/LogRushLogger.hpp
+++ backend/include/Server/logger/LogRushLogger.hpp
@@ -21,13 +21,20 @@ class LogRushBasicSink : public spdlog::sinks::sink
           formatter_(
               spdlog::details::make_unique<spdlog::pattern_formatter>( ) )
     {
-        styles_[spdlog::level::trace] = fmt::fg(fmt::terminal_color::bright_cyan) | fmt::emphasis::bold;
-        styles_[spdlog::level::debug] = fmt::fg(fmt::terminal_color::bright_white) | fmt::emphasis::bold;
-        styles_[spdlog::level::info] = fmt::fg(fmt::terminal_color::bright_green) | fmt::emphasis::bold;
-        styles_[spdlog::level::warn] = fmt::fg(fmt::terminal_color::bright_yellow) | fmt::emphasis::bold;
-        styles_[spdlog::level::err] = fmt::fg(fmt::terminal_color::bright_red) | fmt::emphasis::bold;
-        styles_[spdlog::level::critical] = fmt::fg(fmt::terminal_color::bright_magenta) | fmt::emphasis::bold;  
-        set_color_mode(mode);
+        styles_[ spdlog::level::trace ] =
+            fmt::fg( fmt::terminal_color::bright_cyan ) | fmt::emphasis::bold;
+        styles_[ spdlog::level::debug ] =
+            fmt::fg( fmt::terminal_color::bright_white ) | fmt::emphasis::bold;
+        styles_[ spdlog::level::info ] =
+            fmt::fg( fmt::terminal_color::bright_green ) | fmt::emphasis::bold;
+        styles_[ spdlog::level::warn ] =
+            fmt::fg( fmt::terminal_color::bright_yellow ) | fmt::emphasis::bold;
+        styles_[ spdlog::level::err ] =
+            fmt::fg( fmt::terminal_color::bright_red ) | fmt::emphasis::bold;
+        styles_[ spdlog::level::critical ] =
+            fmt::fg( fmt::terminal_color::bright_magenta ) |
+            fmt::emphasis::bold;
+        set_color_mode( mode );
     }
     LogRushBasicSink( const LogRushBasicSink &other ) = delete;
     ~LogRushBasicSink( ) { client.unregister_stream( log_stream ); }
@@ -38,31 +45,33 @@ class LogRushBasicSink : public spdlog::sinks::sink
         spdlog::memory_buf_t formatted;
         formatter_->format( msg, formatted );
 
-        auto s = std::string(formatted.begin(), formatted.begin() + msg.color_range_start );
+        auto s = std::string( formatted.begin( ),
+                              formatted.begin( ) + msg.color_range_start );
         auto c = fmt::format(
-            styles_[msg.level],
-            std::string_view{ formatted.data() + msg.color_range_start, msg.color_range_end - msg.color_range_start }
-        );
-        auto e = std::string(formatted.begin() + msg.color_range_end, formatted.end() );
+            styles_[ msg.level ],
+            std::string_view{ formatted.data( ) + msg.color_range_start,
+                              msg.color_range_end - msg.color_range_start } );
+        auto e = std::string( formatted.begin( ) + msg.color_range_end,
+                              formatted.end( ) );
 
         log_stream.log( s + c + e );
     }
 
-    void set_color_mode(spdlog::color_mode mode) 
+    void set_color_mode( spdlog::color_mode mode )
     {
-      std::lock_guard lock{ mutex_ };
-      switch (mode) 
-      {
+        std::lock_guard lock{ mutex_ };
+        switch ( mode )
+        {
         case spdlog::color_mode::always:
-          should_color_ = true;
-          break;
+            should_color_ = true;
+            break;
         case spdlog::color_mode::automatic:
-          should_color_ = true;
-          break;
+            should_color_ = true;
+            break;
         case spdlog::color_mode::never:
-          should_color_ = false;
-          break;
-      }
+            should_color_ = false;
+            break;
+        }
     }
 
     void flush( ) {}
