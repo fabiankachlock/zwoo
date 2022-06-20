ee6187246f74c560aaa5e6ede4858e95b92411a2
fabiankachlock <fabian.kachlock@icloud.com>
2022-06-20 20:27:45 +0200

    fix: active game guard now working

--- frontend/src/core/services/security/GameGuard.ts
+++ frontend/src/core/services/security/GameGuard.ts
@@ -1,11 +1,12 @@
 import { useGameConfig } from '@/core/adapter/game';
+import { GameRoute } from '@/router/game';
 // import { GameRoute } from '@/router/game';
 import { RouterInterceptor } from '@/router/types';
 import { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
 import Logger from '../logging/logImport';
 
 export class InGameGuard implements RouterInterceptor {
-  static InGameRoutes: string[] = []; // GameRoute.children?.map(child => `${GameRoute.path ?? ''}/${child?.path ?? ''}`) ?? [];
+  static InGameRoutes: string[] = GameRoute.children?.map(child => `${GameRoute.path ?? ''}/${child?.path ?? ''}`) ?? [];
   private Logger = Logger.RouterGuard.createOne('game');
 
   beforeEach = async (to: RouteLocationNormalized, _from: RouteLocationNormalized, next: NavigationGuardNext): Promise<boolean> => {
@@ -21,7 +22,7 @@ export class InGameGuard implements RouterInterceptor {
           this.Logger.log(`redirecting to login with loopback `);
           next('/login?redirect=' + to.fullPath);
         } else {
-          this.Logger.log(`redirecting to ${redirect}`);
+          this.Logger.log(`redirecting to ${redirect || '/list'}`);
           next(redirect || '/list');
         }
 
