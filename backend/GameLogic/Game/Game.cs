948f7776291eda0c41e6aeac2b470bbd970d3f37
fabiankachlock <fabian.kachlock@icloud.com>
2022-07-01 22:31:46 +0200

    feat: add game logic project

---
+++ backend/GameLogic/Game/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZwooGameLogic;
+
+public struct GameMeta
+{
+    public long Id;
+    public string Name;
+    public bool IsPublic;
+    public bool IsRunning;
+}
+
+public sealed class Game
+{
+    private GameMeta Meta;
+
+    public long Id { get => Meta.Id; }
+    public string Name { get => Meta.Name; }
+    public bool IsPublic { get => Meta.IsPublic; }
+    public bool IsRunning { get => Meta.IsRunning; }
+
+    private GameSettings GameSettings;
+    public GameSettings Settings
+    {
+        get => GameSettings;
+    }
+
+    private PlayerManager PlayerManager;
+    public List<long> AllPlayers { get => PlayerManager.Players; }
+    public int PlayerCount { get => PlayerManager.PlayerCount; }
+
+    public Game(
+        long id,
+        string name,
+        bool isPublic
+    )
+    {
+        Meta = new GameMeta();
+        Meta.Name = name;
+        Meta.Id = id;
+        Meta.IsPublic = isPublic;
+        GameSettings = GameSettings.FromDefaults();
+        PlayerManager = new PlayerManager();
+    }
+
+
+    public bool AddPlayer(long id)
+    {
+        return PlayerManager.AddPlayer(id);
+    }
+
+    public bool RemovePlayer(long id)
+    {
+        return PlayerManager.RemovePlayer(id);
+    }
+
+    public void SetSetting(string key, byte value)
+    {
+        GameSettings.Set(key, value);
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Stop()
+    {
+
+    }
+
+    public void Reset()
+    {
+
+    }
+
+    public void HandleEvent<T>(GameEvent<T> gameEvent)
+    {
+
+    }
+
+}
+
+
---
+++ backend/GameLogic/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using log4net;
+
+namespace ZwooGameLogic;
+
+public sealed class GameManager
+{
+    private readonly ILog _logger;
+
+    private Dictionary<long, Game> ActiveGames;
+    private long GameId;
+
+    public GameManager(ILog logger)
+    {
+        ActiveGames = new Dictionary<long, Game>();
+        GameId = 0;
+        _logger = logger;
+    }
+
+    public Game CreateGame(
+        string name,
+        bool isPublic
+    )
+    {
+        Game newGame = new Game(nextGameId(), name, isPublic);
+        ActiveGames.Add(newGame.Id, newGame);
+        _logger.Info($"Created Game ${newGame.Id}");
+        return newGame;
+    }
+
+    public Game? GetGame(long id)
+    {
+        if (ActiveGames.ContainsKey(id))
+        {
+            return ActiveGames[id];
+        }
+        _logger.Debug($"Game find game ${id}");
+        return null;
+    }
+
+    public bool RemoveGame(long id)
+    {
+        _logger.Debug($"Removing game {id}");
+        return ActiveGames.Remove(id);
+    }
+
+    public List<Game> FindGames(string search)
+    {
+        return ActiveGames
+            .Where(pair => pair.Key.ToString().Contains(search) || pair.Value.Name.Contains(search))
+            .Select(pair => pair.Value)
+            .ToList();
+    }
+
+    public List<Game> GetAllGames()
+    {
+        return ActiveGames
+            .Select(pair => pair.Value)
+            .ToList();
+    }
+
+    private long nextGameId()
+    {
+        return ++GameId;
+    }
+
+
+
+}
