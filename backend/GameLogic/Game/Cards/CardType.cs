de2b0a18e54606e256b4b9baf3ba47b653d616fe
fabiankachlock <fabian.kachlock@icloud.com>
2022-07-01 23:25:27 +0200

    feat: add cards

---
+++ backend/GameLogic/Game/Cards/CardType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZwooGameLogic.Game.Cards;
+
+public enum CardType
+{
+    None = 0,
+    Zero = 1,
+    One = 2,
+    Two = 3,
+    Three = 4,
+    Four = 5,
+    Five = 6,
+    Six = 7,
+    Seven = 8,
+    Eight = 9,
+    Nine = 10,
+    Skip = 11,
+    Reverse = 12,
+    DrawTwo = 13,
+    Wild = 14,
+    WildFour = 15
+}
