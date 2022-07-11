fd3bae09e284d92b6c3c1339fbcf8c98fd3b7907
fabiankachlock <fabian.kachlock@icloud.com>
2022-07-11 20:57:33 +0200

    feat: add zrp decoding & encoding

---
+++ backend/ZwooBackend/ZRP/ZRPDecoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ZwooBackend.ZRP;
+
+public class ZRPDecoder
+{
+    private static JsonSerializerOptions _options = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    static public (ZRPCode?, T?) Decode<T>(string msg)
+    {
+        int index = msg.IndexOf(",");
+        int code = Convert.ToInt32(msg.Substring(0, index));
+        string payload = msg.Substring(index + 1);
+        return (Enum.IsDefined(typeof(ZRPCode), code) ? (ZRPCode)code : null, JsonSerializer.Deserialize<T>(payload, _options));
+    }
+}
---
+++ backend/ZwooBackend/ZRP/ZRPEncoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ZwooBackend.ZRP;
+
+public class ZRPEncoder
+{
+    private static JsonSerializerOptions _options = new JsonSerializerOptions()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    static public string Encode<T>(ZRPCode code, T payload)
+    {
+        return $"{code},{JsonSerializer.Serialize(payload, ZRPEncoder._options)}";
+    }
+}
