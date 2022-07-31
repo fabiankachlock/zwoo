dd3485958d2832cde3d0d5045e36b668d40f179b
fabiankachlock <fabian.kachlock@icloud.com>
2022-07-31 19:49:52 +0200

    feat: skip card rule

---
+++ backend/GameLogic/Game/Rules/SkipRule.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZwooGameLogic.Game.Events;
+using ZwooGameLogic.Game.Settings;
+using ZwooGameLogic.Game.State;
+using ZwooGameLogic.Game.Cards;
+
+namespace ZwooGameLogic.Game.Rules;
+
+internal class SkipCardRule : BaseCardRule
+{
+    public override int Priority
+    {
+        get => RulePriorirty.DefaultRule;
+    }
+
+    public override string Name
+    {
+        get => "BaseCardRule";
+    }
+
+    public override GameSettingsKey? AssociatedOption
+    {
+        get => GameSettingsKey.DEFAULT_RULE_SET;
+    }
+
+    public SkipCardRule() : base() { }
+
+    public override bool IsResponsible(ClientEvent gameEvent, GameState state)
+    {
+        return gameEvent.Type == ClientEventType.PlaceCard && gameEvent.CastPayload<ClientEvent.PlaceCardEvent>().Card.Type == CardType.Skip;
+    }
+
+
+    public override GameStateUpdate ApplyRule(ClientEvent gameEvent, GameState state, Pile cardPile, PlayerCycle playerOrder)
+    {
+        if (!IsResponsible(gameEvent, state)) return GameStateUpdate.None(state);
+        List<GameEvent> events = new List<GameEvent>();
+
+        ClientEvent.PlaceCardEvent payload = gameEvent.CastPayload<ClientEvent.PlaceCardEvent>();
+        bool isAllowed = CanThrowCard(state.TopCard.Card, payload.Card) && !(CardUtilities.IsDraw(state.TopCard.Card) && !state.TopCard.EventActivated);
+        if (IsActivePlayer(state, payload.Player) && isAllowed && PlayerHasCard(state, payload.Player, payload.Card))
+        {
+            state = PlayPlayerCard(state, payload.Player, payload.Card);
+            (state, events) = ChangeActivePlayerByAmount(state, playerOrder, 2);
+            events.Add(GameEvent.RemoveCard(payload.Player, payload.Card));
+            return new GameStateUpdate(state, events);
+        }
+
+        return GameStateUpdate.WithEvents(state, new List<GameEvent>() { GameEvent.Error(payload.Player, GameError.CantPlaceCard) });
+    }
+
+    // Rule utilities
+    protected (GameState, List<GameEvent>) ChangeActivePlayerByAmount(GameState state, PlayerCycle playerOrder, int amount)
+    {
+        long nextPlayer = playerOrder.Next();
+        if (amount > 1)
+        {
+            for (int i = 0; i < amount - 1; i++)
+            {
+                nextPlayer = playerOrder.Next();
+            }
+        }
+
+        List<GameEvent> events = new List<GameEvent>() {
+            GameEvent.EndTurn(state.CurrentPlayer),
+            GameEvent.StartTurn(nextPlayer),
+        };
+        state.CurrentPlayer = nextPlayer;
+        return (state, events);
+    }
+}
