6fe6cb1bf6717237faba84f20f32ce331e859e27
fabiankachlock <fabian.kachlock@icloud.com>
2022-09-01 21:06:17 +0200

    fix: login page styling

--- frontend/src/views/404.vue
+++ frontend/src/views/404.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="grid w-full h-full place-items-center p-10">
-    <p class="text-6xl tc-main text-center">Oooops 404</p>
-    <router-link to="/" class="tc-main-secondary italic text-2xl">>> zwoo >></router-link>
+  <div class="max-height-ios">
+    <div class="grid w-full place-items-center p-10">
+      <p class="text-6xl tc-main text-center">Oooops 404</p>
+      <router-link to="/" class="tc-main-secondary italic text-2xl">>> zwoo >></router-link>
+    </div>
   </div>
 </template>
