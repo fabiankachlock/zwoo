8e9366494c8d1cb1bc645bd5038264570562bea0
Visual-Rock <visualrock.5329@gmail.com>
2022-09-05 20:03:58 +0200

    add: Docker + Player Quick Find + Deleted Accounts

--- backend/ZwooInfoDashBoard/Data/User.cs
+++ backend/ZwooInfoDashBoard/Data/User.cs
@@ -21,44 +21,36 @@ public class User
         Verified = verified;
     }
 
-    [JsonIgnore]
     [BsonElement("_id")]
     public UInt64 Id { set; get; } = 0;
     
     [BsonElement("sid")]
-    [JsonIgnore]
     [BsonIgnoreIfDefault]
     public string Sid { set; get; } = "";
     
-    [JsonPropertyName("username")]
     [BsonElement("username")]
     public string Username { set; get; } = "";
     
-    [JsonPropertyName("email")]
     [BsonElement("email")]
     public string Email { set; get; } = "";
     
-    [JsonIgnore]
     [BsonElement("password")]
     public string Password { set; get; } = "";
     
-    [JsonPropertyName("wins")]
     [BsonElement("wins")]
     public UInt32 Wins { set; get; } = 0;
     
     [BsonElement("validation_code")]
-    [JsonIgnore]
     [BsonIgnoreIfDefault]
     public string ValidationCode { set; get; } = "";
     
-    [JsonIgnore]
     [BsonElement("verified")]
     public bool Verified { set; get; } = false;
     
     [BsonElement("beta_code")]
-    [JsonIgnore]
     [BsonIgnoreIfDefault]
     public string? BetaCode { set; get; }
 
+    [BsonIgnore]
     public int Position { set; get; } = -1;
 }
