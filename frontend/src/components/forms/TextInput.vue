b65b4b50f725201ad34e2825e158cd00b7adeefa
fabiankachlock <fabian.kachlock@icloud.com>
2022-09-27 12:22:17 +0200

    #265 feat: add password toggle button

--- frontend/src/components/forms/TextInput.vue
+++ frontend/src/components/forms/TextInput.vue
@@ -2,22 +2,32 @@
   <div class="w-full mb-4">
     <div class="m-2">
       <label class="block tc-main-secondary text-sm font-bold mb-2" :for="id">{{ t(labelKey) }}</label>
-      <input
-        :ref="
+      <div class="relative">
+        <input
+          :ref="
         r => {
           input = r as HTMLInputElement;
         }
       "
-        autocomplete=""
-        class="bg-dark shadow appearance-none border bc-main rounded w-full py-2 px-3 tc-main-light leading-tight focus:outline-none focus:shadow-outline focus:bc-primary focus:bg-darkest"
-        :name="id"
-        :type="isPassword ? 'password' : 'text'"
-        :id="id"
-        :placeholder="placeholder"
-        :value="modelValue"
-        @keyup.stop
-        @input="updateInput"
-      />
+          autocomplete=""
+          class="bg-dark shadow appearance-none border bc-main rounded w-full py-2 pl-3 pr-7 tc-main-light leading-tight focus:outline-none focus:shadow-outline focus:bc-primary focus:bg-darkest"
+          :name="id"
+          :type="isPassword ? (isPasswordVisible ? 'password' : 'text') : 'text'"
+          :id="id"
+          :placeholder="placeholder"
+          :value="modelValue"
+          @keyup.stop
+          @input="updateInput"
+        />
+        <button
+          v-if="isPassword"
+          @click.stop.prevent="togglePasswordMode"
+          class="absolute right-2 top-1/2 transform -translate-y-1/2 tc-main text-lg"
+        >
+          <Icon icon="mdi:eye-outline" v-show="!isPasswordVisible" />
+          <Icon icon="mdi:eye-off-outline" v-show="isPasswordVisible" />
+        </button>
+      </div>
       <div class="my-2">
         <Error v-show="!isValid" :errors="error" />
       </div>
@@ -29,6 +39,7 @@
 import { Validator } from '@/core/services/validator/_type';
 import { defineEmits, defineProps, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { Icon } from '@iconify/vue';
 import Error from '../misc/Error.vue';
 
 const props = defineProps<{
@@ -47,9 +58,14 @@ const emit = defineEmits<{
 
 const input = ref<HTMLInputElement>();
 const isValid = ref<boolean>(true);
+const isPasswordVisible = ref<boolean>(false);
 const error = ref<string[]>([]);
 
 const { t } = useI18n();
+
+const togglePasswordMode = () => {
+  isPasswordVisible.value = !isPasswordVisible.value;
+};
 
 const updateInput = () => {
   const newValue = input.value?.value || '';
