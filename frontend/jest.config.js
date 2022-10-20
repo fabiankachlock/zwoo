aad343e6a82be1ce0c76a0447ba68c74b862f870
fabiankachlock <fabian.kachlock@icloud.com>
2022-10-20 20:14:54 +0200

    ZWOO-114 feat: enable unit tests with jest

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   verbose: true,
-
+  testMatch: ['**/tests/unit/**/*.spec.[jt]s?(x)', '**/__tests__/*.[jt]s?(x)'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'vue'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: true
+    }
+  },
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.ts$': 'ts-jest'
