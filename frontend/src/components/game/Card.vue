7adf79c33b07aa6db9cd4c20f03894919d6c8ff3
fabiankachlock <fabian.kachlock@icloud.com>
2022-10-21 19:04:50 +0200

    ZWOO-114 fix: imports

--- frontend/src/components/game/Card.vue
+++ frontend/src/components/game/Card.vue
@@ -18,7 +18,7 @@ import { computed, defineProps } from 'vue';
 import { useCardTheme } from '@/core/adapter/play/cardTheme';
 import { CardTheme } from '@/core/services/cards/CardTheme';
 import { CardDescriptor } from '@/core/services/cards/CardThemeConfig';
-import { Card } from '@/core/services/game/CardTypesTypes';
+import { Card } from '@/core/services/game/CardTypes';
 
 const props = defineProps<{
   card:
