f1aa2fca6f7a22de7135faf0bc5742a573a44654
fabiankachlock <fabian.kachlock@icloud.com>
2022-10-29 20:56:44 +0200

    ZWOO-100 fix: button design

--- frontend/src/views/MissingCookies.vue
+++ frontend/src/views/MissingCookies.vue
@@ -4,7 +4,7 @@
     <p class="tc-main-secondary text-center">{{ t('cookies.disallowed.info') }}</p>
     <div class="flex justify-center items-center my-2">
       <button
-        class="flex justify-center items-center bg-lightest border-2 border-transparent px-4 py-1 rounded transition hover:bg-light cursor-pointer select-none"
+        class="flex justify-center items-center bg-dark border-2 border-transparent px-4 py-1 rounded transition hover:bg-darkest cursor-pointer select-none"
         @click="manageSelection"
       >
         <p class="tc-main-secondary text-center">{{ t('cookies.manage') }}</p>
