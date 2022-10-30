f3922de499c89268a31b49045769242326bc7ae6
fabiankachlock <fabian.kachlock@icloud.com>
2022-10-30 23:45:47 +0100

    ZWOO-110 remove generated icons from git

--- frontend/src/modules/zwoo-icons/icons/akar-icons:cross.js
+++
@@ -1,1 +0,0 @@
-export default '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 20L4 4m16 0L4 20"/></svg>'
--- frontend/src/modules/zwoo-icons/icons/iconoir:eye-alt.js
+++
@@ -1,1 +0,0 @@
-export default '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M4.5 12.5c3-6.5 12-6.5 15 0"/><path d="M12 16a2 2 0 1 1 0-4a2 2 0 0 1 0 4Z"/></g></svg>'
