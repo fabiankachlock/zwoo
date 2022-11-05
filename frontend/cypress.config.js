efd495b4a928c078b0d1a983f0e881b372e5a50b
fabiankachlock <fabian.kachlock@icloud.com>
2022-11-05 21:42:51 +0100

    feat: reacord cypress ci runs

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: '1yq2ff',
   fixturesFolder: 'tests/e2e/fixtures',
   screenshotsFolder: 'tests/e2e/screenshots',
   videosFolder: 'tests/e2e/videos',
