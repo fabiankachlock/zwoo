247cd533dd8a8865b026a7d7d828fb66f385d783
fabiankachlock <fabian.kachlock@icloud.com>
2022-11-11 18:41:12 +0100

    ZWOO-95 feat: make settings updates working again

--- frontend/src/core/services/__mocks__/server.js
+++ frontend/src/core/services/__mocks__/server.js
@@ -2,11 +2,11 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   if (req.url.startsWith('/version')) {
-    res.writeHead(200, { 'Access-Control-Allow-Origin': 'http://localhost:15761' });
-    res.end('1.0.0-beta.12');
+    res.writeHead(200, { 'Access-Control-Allow-Origin': 'http://localhost:15771' });
+    res.end('1.0.0-beta.7');
     return;
   }
-  res.writeHead(404, { 'Access-Control-Allow-Origin': 'http://localhost:15761' });
+  res.writeHead(404, { 'Access-Control-Allow-Origin': 'http://localhost:15771' });
   res.end('not found');
 };
 
