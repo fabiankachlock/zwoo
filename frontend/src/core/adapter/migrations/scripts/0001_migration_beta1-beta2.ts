420a3cf93027cafe42251eca8a90830f10c33f5f
fabiankachlock <fabian.kachlock@icloud.com>
2022-11-17 20:27:44 +0100

    ZWOO-164 fix: rethinking migrations

--- frontend/src/core/adapter/migrations/scripts/0001_migration_beta1-beta2.ts
+++ frontend/src/core/adapter/migrations/scripts/0001_migration_beta1-beta2.ts
@@ -1,13 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import { useConfig } from '@/core/adapter/config';
+import { ZwooConfigKey } from '@/core/adapter/config';
 import { defaultLanguage, supportedLanguages } from '@/i18n';
 
 export default {
   up() {
     const userLng = navigator.language.split('-')[0]?.toLowerCase();
     const uiLng = supportedLanguages.includes(userLng) ? userLng : defaultLanguage;
-    useConfig().setLanguage(uiLng);
+    useConfig().set(ZwooConfigKey.Language, uiLng);
     return;
   },
   down() {
