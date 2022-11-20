80df392b1dc866beadd0f41d7e468a4e3cafc906
fabiankachlock <fabian.kachlock@icloud.com>
2022-11-20 20:10:41 +0100

    ZWOO-167 fix: consistend font color in settings

--- frontend/src/components/settings/ManageCookies.vue
+++ frontend/src/components/settings/ManageCookies.vue
@@ -3,7 +3,7 @@
     class="flex justify-center items-center bg-light border-2 border-transparent px-2 rounded transition hover:bg-main cursor-pointer select-none"
     @click="manageCookies"
   >
-    <p class="tc-main-secondary text-center">{{ t('cookies.manage') }}</p>
+    <p class="tc-main-light text-center">{{ t('cookies.manage') }}</p>
   </button>
 </template>
 
