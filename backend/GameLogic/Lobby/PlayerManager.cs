3aa94f6d914b87e3ce620147c781833234d889fa
fabiankachlock <fabian.kachlock@icloud.com>
2022-12-17 20:07:41 +0100

    ZWOO-195 feat(wip): fix PlayerManager

--- backend/GameLogic/Lobby/PlayerManager.cs
+++ backend/GameLogic/Lobby/PlayerManager.cs
@@ -1,50 +1,47 @@
-﻿using Microsoft.AspNetCore;
-using ZwooBackend.Games;
-using ZwooBackend.Websockets.Interfaces;
-using ZwooBackend.ZRP;
-using ZwooGameLogic.Game;
-using static ZwooBackend.Globals;
+﻿using ZwooGameLogic.ZRP;
+using log4net;
 
-namespace GameLogic.Lobby;
+namespace ZwooGameLogic.Lobby;
 
 // should this be in gamelogic?
 public class PlayerManager
 {
+    // TODO: cleanup - this should be managed twice
     private Dictionary<long, HashSet<long>> _games = new Dictionary<long, HashSet<long>>();
-    private SendableWebSocketManager _webSocketManager;
+    private INotificationAdapter _webSocketManager;
+    private GameManager _gameManager;
+    private ILog _logger;
 
-    public PlayerManager(SendableWebSocketManager webSocketManager)
+    public PlayerManager(INotificationAdapter webSocketManager, GameManager gameManager)
     {
         _webSocketManager = webSocketManager;
+        _gameManager = gameManager;
+        _logger = LogManager.GetLogger("PlayerManager");
     }
 
-    public async Task<(LobbyManager.PlayerEntry?, GameRecord?)> ConnectPlayer(long playerId, long gameId)
+    public async Task ConnectPlayer(long playerId, long gameId)
     {
-        GameRecord? game = GameManager.Global.GetGame(gameId);
+        ZwooRoom? game = _gameManager.GetGame(gameId);
         var player = game?.Lobby.GetPlayer(playerId);
-        if (game == null || player == null)
-        {
-            return (null, null);
-        }
+        if (game == null || player == null) return;
 
-        WebSocketLogger.Info($"[PlayerManager] {playerId} connected");
+        _logger.Info($"{playerId} connected");
         game?.Lobby.PlayerConnected(playerId);
         if (player.Role == ZRPRole.Spectator)
         {
             // TODO: change player model to include wins
-            await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.SpectatorJoined, new SpectatorJoinedDTO(player.Username, 0)));
+            await _webSocketManager.BroadcastGame(gameId, ZRPCode.SpectatorJoined, new SpectatorJoinedDTO(player.Username, 0));
         }
         else
         {
-            await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.PlayerReconnected, new PlayerReconnectedDTO(player.Username)));
-            await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.PlayerJoined, new PlayerJoinedDTO(player.Username, 0)));
+            await _webSocketManager.BroadcastGame(gameId, ZRPCode.PlayerReconnected, new PlayerReconnectedDTO(player.Username));
+            await _webSocketManager.BroadcastGame(gameId, ZRPCode.PlayerJoined, new PlayerJoinedDTO(player.Username, 0));
         }
-        return (player, game);
     }
 
     public async Task DisconnectPlayer(long playerId, long gameId)
     {
-        GameRecord? game = GameManager.Global.GetGame(gameId);
+        ZwooRoom? game = _gameManager.GetGame(gameId);
         var player = game?.Lobby.GetPlayer(playerId);
         if (player == null)
         {
@@ -57,8 +54,8 @@ public class PlayerManager
 
             if (game.Game.IsRunning)
             {
-                WebSocketLogger.Info($"[PlayerManager] {playerId} disconnected from running game");
-                await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.PlayerDisconnected, new PlayerDisconnectedDTO(player.Username)));
+                _logger.Info($"{playerId} disconnected from running game");
+                await _webSocketManager.BroadcastGame(gameId, ZRPCode.PlayerDisconnected, new PlayerDisconnectedDTO(player.Username));
                 game?.Lobby.PlayerDisconnected(playerId);
 
                 if (player.Role == ZRPRole.Host)
@@ -66,9 +63,9 @@ public class PlayerManager
                     var newHost = game!.Lobby.SelectNewHost();
                     if (newHost != null)
                     {
-                        await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.PlayerChangedRole, new PlayerChangedRoleDTO(newHost.Username, ZRPRole.Host, 0)));
-                        await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.HostChanged, new HostChangedDTO(newHost.Username)));
-                        await _webSocketManager.SendPlayer(newHost.Id, ZRPEncoder.EncodeToBytes(ZRPCode.PromotedToHost, new PromotedToHostDTO()));
+                        await _webSocketManager.BroadcastGame(gameId, ZRPCode.PlayerChangedRole, new PlayerChangedRoleDTO(newHost.Username, ZRPRole.Host, 0));
+                        await _webSocketManager.BroadcastGame(gameId, ZRPCode.HostChanged, new HostChangedDTO(newHost.Username));
+                        await _webSocketManager.SendPlayer(newHost.Id, ZRPCode.PromotedToHost, new PromotedToHostDTO());
                     }
                     else
                     {
@@ -79,35 +76,35 @@ public class PlayerManager
             }
             else
             {
-                WebSocketLogger.Info($"[PlayerManager] {playerId} removed from lobby");
+                _logger.Info($"{playerId} removed from lobby");
                 playerRemoveResult = game.Lobby.RemovePlayer(playerId);
 
                 // only send leave message when the player leaves (NOT disconnects)
                 if (player.Role == ZRPRole.Spectator)
                 {
                     // TODO: change player model to include wins
-                    await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.SpectatorLeft, new SpectatorLeftDTO(player.Username)));
+                    await _webSocketManager.BroadcastGame(gameId, ZRPCode.SpectatorLeft, new SpectatorLeftDTO(player.Username));
                 }
                 else
                 {
-                    await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.PlayerLeft, new PlayerLeftDTO(player.Username)));
+                    await _webSocketManager.BroadcastGame(gameId, ZRPCode.PlayerLeft, new PlayerLeftDTO(player.Username));
                 }
             }
 
-
-            WebSocketLogger.Info(game.Lobby.ListAll());
+            if (game == null) return;
+            _logger.Info(game.Lobby.ListAll());
             if (game?.Lobby.ActivePlayerCount() == 0 || game.Game.IsRunning && game?.Lobby.PlayerCount() < 2 || playerRemoveResult == LobbyResult.Error)
             {
-                WebSocketLogger.Info($"force closing game {gameId} due to a lack of players");
-                await _webSocketManager.QuitGame(gameId);
-                GameManager.Global.RemoveGame(gameId);
+                _logger.Info($"force closing game {gameId} due to a lack of players");
+                await _webSocketManager.DisconnectGame(gameId);
+                _gameManager.RemoveGame(gameId);
             }
         }
     }
 
     public async Task FinishGame(long gameId)
     {
-        GameRecord? game = GameManager.Global.GetGame(gameId);
+        ZwooRoom? game = _gameManager.GetGame(gameId);
         if (game == null)
         {
             return;
@@ -123,7 +120,7 @@ public class PlayerManager
         {
             // make them a spectator
             var result = game.Lobby.ChangeRole(player.Username, ZRPRole.Spectator);
-            await _webSocketManager.BroadcastGame(gameId, ZRPEncoder.EncodeToBytes(ZRPCode.PlayerChangedRole, new PlayerChangedRoleDTO(player.Username, ZRPRole.Spectator, 0)));
+            await _webSocketManager.BroadcastGame(gameId, ZRPCode.PlayerChangedRole, new PlayerChangedRoleDTO(player.Username, ZRPRole.Spectator, 0));
         }
 
         game.Lobby.ResetDisconnectedStates();
