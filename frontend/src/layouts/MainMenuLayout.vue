5daa3b5d4595e0b9b0c5c64371c5f185d7d56d6d
fabiankachlock <fabian.kachlock@icloud.com>
2023-02-03 22:42:26 +0100

    ZWOO-228 feat: frontend layout footer

--- frontend/src/layouts/MainMenuLayout.vue
+++ frontend/src/layouts/MainMenuLayout.vue
@@ -15,7 +15,7 @@
       <FloatingMenu />
     </div>
     <div class="main-content">
-      <div class="pt-2 mt-10 relative">
+      <div class="pt-2 pb-2 mt-10 mb-8 relative">
         <div class="w-full sticky z-10 bg-main top-10">
           <slot name="sticky-content"></slot>
         </div>
@@ -25,6 +25,24 @@
         <!-- workaround for ios hidden content on the end of the side -->
         <div class="h-14"></div>
       </div>
+      <div class="footer fixed bottom-0 left-0 right-0 bg-darkest h-8 bc-primary border-t-1 z-30">
+        <footer class="h-full">
+          <div class="h-8 py-1 flex justify-between items-center mx-3">
+            <router-link to="/">
+              <Icon icon="mi:home" class="text-xl tc-main" />
+            </router-link>
+            <div class="flex py-1">
+              <span class="footer-item">© IGD20 {{ year }}</span>
+              <router-link to="/imprint" class="footer-item">Imprint</router-link>
+              <router-link to="/privacy" class="footer-item">Privacy</router-link>
+            </div>
+            <button class="flex items-center">
+              <Icon icon="material-symbols:help-outline-rounded" class="text-xl tc-main mr-1" />
+              <span class="tc-main"> Help </span>
+            </button>
+          </div>
+        </footer>
+      </div>
     </div>
   </div>
 </template>
@@ -32,12 +50,28 @@
 <script setup lang="ts">
 import FloatingMenu from '@/components/menu/FloatingMenu.vue';
 import NavBar from '@/components/menu/NavBar.vue';
+import { Icon } from '@/components/misc/Icon';
+
+const year = new Date().getFullYear();
 </script>
 
 <style lang="css" scoped>
 /* notch phone things */
 .header {
   padding: env(safe-area-inset-top) env(safe-area-inset-right) 0 env(safe-area-inset-left);
+}
+
+.footer-item:not(:last-child) {
+  @apply border-r bc-lightest px-3;
+}
+
+a.footer-item {
+  text-decoration: underline;
+}
+
+.footer-item,
+.footer-item:last-child {
+  @apply px-3 tc-main-secondary;
 }
 
 .main-content {
--- frontend/src/modules/zwoo-icons/generate-icons.js
+++ frontend/src/modules/zwoo-icons/generate-icons.js
@@ -54,7 +54,8 @@ const icons = [
   'mdi:cloud-sync-outline',
   'mdi:cloud-off-outline',
   'fluent:bot-add-20-regular',
-  'fluent:bot-24-regular'
+  'fluent:bot-24-regular',
+  'material-symbols:help-outline-rounded'
 ];
 
 const { SVG, Collection } = require('@iconify/json-tools');
