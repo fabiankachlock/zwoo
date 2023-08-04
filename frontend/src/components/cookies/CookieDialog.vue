19f4f5c72d068acba14ad60f4b77391f413eb93a
fabiankachlock <fabian.kachlock@icloud.com>
2023-08-04 23:02:14 +0200

    ZWOO-328 feat: improve Cookie Dialog to be TTDSG conform

--- frontend/src/components/cookies/CookieDialog.vue
+++ frontend/src/components/cookies/CookieDialog.vue
@@ -1,44 +1,71 @@
 <template>
   <FloatingDialog>
     <div class="flex flex-col mx-2 overflow-y-auto">
-      <h2 class="tc-main text-xl text-center my-2">{{ t('cookies.title') }}</h2>
-      <div class="bc-darkest border-t border-b py-1">
-        <h3 class="tc-main text-lg">{{ t('cookies.functional.title') }}</h3>
-        <ul class="ml-4 list">
-          <li class="bc-main">
-            <div>
-              <h4 class="text tc-main">{{ t('cookies.functional.auth.name') }}</h4>
-              <p class="text-xs tc-main-secondary ml-2 mb-1">{{ t('cookies.functional.auth.description') }}</p>
+      <h2 class="tc-main text-xl text-center mt-2">{{ t('cookies.title') }}</h2>
+      <p class="tc-main-secondary mb-2">{{ t('cookies.userInformation') }}</p>
+      <div class="py-1">
+        <div class="flex justify-between items-center bg-light rounded p-1" @click="toggle('functional')">
+          <h3 class="tc-main text-lg ml-1">{{ t('cookies.functional.title') }}</h3>
+          <button class="text-2xl tc-main hover:tc-primary relative p-3 rounded w-6 h-4 overflow-hidden">
+            <Icon
+              icon="iconoir:nav-arrow-down"
+              class="icon absolute left-1/2 top-1/2 -translate-x-1/2 transition duration-300"
+              :class="{ 'opacity-0 translate-y-2': isOpen('functional'), '-translate-y-1/2': !isOpen('functional') }"
+            />
+            <Icon
+              icon="iconoir:nav-arrow-up"
+              class="icon absolute left-1/2 top-1/2 -translate-x-1/2 transition duration-300"
+              :class="{ 'opacity-0 translate-y-2': !isOpen('functional'), '-translate-y-1/2': isOpen('functional') }"
+            />
+          </button>
+        </div>
+
+        <div
+          class="relative grid overflow-hidden grid-rows-[0fr] transition-[grid-template-rows]"
+          :class="{ 'grid-rows-[1fr]': isOpen('functional') }"
+        >
+          <div class="min-h-0">
+            <div class="item">
+              <div>
+                <h4 class="text tc-main">{{ t('cookies.functional.auth.name') }}</h4>
+                <p class="text-xs tc-main-secondary mb-1">{{ t('cookies.functional.auth.description') }}</p>
+              </div>
+              <div class="bg-main border-2 border-transparent px-4 py-1 ml-4 rounded cursor-not-allowed select-none">
+                <p class="tc-main-secondary flex flex-row flex-nowrap justify-end items-center">
+                  <span>{{ t('cookies.active') }}</span>
+                  <Icon icon="akar-icons:check" class="ml-2" />
+                </p>
+              </div>
             </div>
-            <div class="bg-main border-2 border-transparent px-4 py-1 ml-4 rounded cursor-not-allowed select-none">
-              <p class="tc-main-secondary flex flex-row flex-nowrap justify-end items-center">
-                <span>{{ t('cookies.active') }}</span>
-                <Icon icon="akar-icons:check" class="ml-2" />
-              </p>
-            </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
       <div class="grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-2 mt-4">
+        <div></div>
+        <button class="flex justify-center items-center bg-main px-4 py-1 rounded transition hover:bg-dark cursor-pointer select-none" @click="close">
+          <p class="tc-main-secondary text-center">{{ t('cookies.__close') }}</p>
+        </button>
+      </div>
+      <!-- <div class="grid grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-2 mt-4">
         <button
-          class="flex justify-center items-center bg-light border-2 border-transparent px-4 py-1 rounded transition hover:bg-main cursor-pointer select-none"
+          class="flex justify-center items-center bg-main px-4 py-1 rounded transition hover:bg-dark cursor-pointer select-none"
           @click="rejectAll"
         >
           <p class="tc-main-secondary text-center">{{ t('cookies.rejectAll') }}</p>
         </button>
         <button
-          class="flex justify-center items-center bg-main border-2 border-primary px-4 py-1 rounded transition hover:bg-main cursor-pointer select-none"
+          class="flex justify-center items-center bg-main px-4 py-1 rounded transition hover:bg-dark cursor-pointer select-none"
           @click="acceptSelection"
         >
-          <p class="tc-primary text-center">{{ t('cookies.acceptSelection') }}</p>
+          <p class="tc-main-secondary text-center">{{ t('cookies.acceptSelection') }}</p>
         </button>
-      </div>
+      </div> -->
     </div>
   </FloatingDialog>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, reactive } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import { Icon } from '@/components/misc/Icon';
@@ -48,26 +75,34 @@ import FloatingDialog from '../misc/FloatingDialog.vue';
 
 const cookies = useCookies();
 const { t } = useI18n();
-
+const sectionsOpen = reactive<Record<string, boolean>>({});
 // const recaptchaActivated = ref(true);
+
+const isOpen = (key: string) => sectionsOpen[key];
+
+const toggle = (key: string) => {
+  sectionsOpen[key] = !sectionsOpen[key];
+};
 
 onMounted(() => {
   // recaptchaActivated.value = cookies.recaptchaCookie;
 });
 
-const acceptSelection = () => {
-  // cookies.setReCaptchaCookie(recaptchaActivated.value);
-  cookies.didShowDialog();
-};
+// const acceptSelection = () => {
+//   // cookies.setReCaptchaCookie(recaptchaActivated.value);
+//   cookies.didShowDialog();
+// };
 
-const rejectAll = () => {
-  cookies.rejectAll();
-  cookies.didShowDialog();
-};
+// const rejectAll = () => {
+//   cookies.rejectAll();
+//   cookies.didShowDialog();
+// };
+
+const close = () => cookies.didShowDialog();
 </script>
 
 <style scoped>
-.list li {
-  @apply flex flex-row flex-nowrap justify-between items-center my-1 border-b;
+.item {
+  @apply flex flex-row flex-nowrap justify-between items-start my-1 ml-4;
 }
 </style>
