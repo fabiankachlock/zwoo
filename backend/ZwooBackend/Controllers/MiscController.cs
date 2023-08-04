afddbb1509f3637004c579cbce3416c78133b558
fabiankachlock <fabian.kachlock@icloud.com>
2023-08-04 09:50:07 +0200

    ZWOO-331 fix: send contact email with correct time

--- backend/ZwooBackend/Controllers/MiscController.cs
+++ backend/ZwooBackend/Controllers/MiscController.cs
@@ -76,7 +76,7 @@ public class MiscController : Controller
             Origin = body.Site,
             CaptchaScore = captchaResponse.Score,
         };
-        _contactRequests.CreateRequest(request);
+        request = _contactRequests.CreateRequest(request);
         if (request.CaptchaScore >= 0.5)
         {
             _emailService.SendContactFormEmail(_emailService.CreateRecipient(ContactEmail, "ZwooBackend", LanguageCode.English), request);
