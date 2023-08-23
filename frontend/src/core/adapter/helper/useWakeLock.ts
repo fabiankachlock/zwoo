e92f91649091abe80c1d8041505777f8f51db265
fabiankachlock <fabian.kachlock@icloud.com>
2023-08-23 15:50:26 +0200

    fix: ts errors

--- frontend/src/core/adapter/helper/useWakeLock.ts
+++ frontend/src/core/adapter/helper/useWakeLock.ts
@@ -1,28 +1,8 @@
 import Logger from '@/core/services/logging/logImport';
 
-// TODO: ts definitions
-interface WakeLock {
-  request(type: string): Promise<WakeLockSentinel>;
-}
-
-interface WakeLockSentinel {
-  release(): Promise<void>;
-  readonly onrelease: (event: Event) => unknown;
-  readonly type: WakeLockType;
-  addEventListener(event: string, callback: EventListenerOrEventListenerObject, options?: unknown): void;
-  removeEventListener(event: string, callback: EventListenerOrEventListenerObject, options?: unknown): void;
-}
-
-type WakeLockType = 'screen' | null;
-
-interface NavigatorExtended extends Navigator {
-  // Only available in a secure context.
-  readonly wakeLock: WakeLock;
-}
-
 export const useWakeLock = async (): Promise<(() => void) | undefined> => {
   try {
-    const wakeLock = await (navigator as NavigatorExtended).wakeLock.request('screen');
+    const wakeLock = await navigator.wakeLock.request('screen');
     if (wakeLock) {
       Logger.debug(`WakeLock instantiated: ${wakeLock.type}`);
     }
