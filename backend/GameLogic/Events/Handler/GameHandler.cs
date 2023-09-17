e0c78d1b3b2da0b593d775caa457030bcc9527f1
fabiankachlock <fabian.kachlock@icloud.com>
2023-09-17 19:59:21 +0200

    ZWOO-200 fix: remove regression for spectators from ZWOO-317

--- backend/GameLogic/Events/Handler/GameHandler.cs
+++ backend/GameLogic/Events/Handler/GameHandler.cs
@@ -47,6 +47,7 @@ public class GameHandler : IUserEventHandler
             ));
         }
 
+        websocketManager.BroadcastGame(context.GameId, ZRPCode.GameStarted, new GameStartedNotification());
         websocketManager.SendPlayer(context.Game.State.ActivePlayer(), ZRPCode.StartTurn, new StartTurnNotification());
     }
 
