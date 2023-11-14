b1887b4200d933a5fffa4724ef79a8d9bd51a87e
fabiankachlock <fabian.kachlock@icloud.com>
2023-11-14 13:56:49 +0100

    ZWOO-400 feat: add local game profile adapter in frontend

---
+++ frontend/src/core/services/gameProfileCache/LocalGameProfileManager.ts
@@ -0,0 +1,49 @@
+import type { BaseLogger } from '@/core/services/logging/logTypes';
+
+export type LocalGameProfile = {
+  Id: string;
+  Name: string;
+  Settings: {
+    Settings: Record<string, number>;
+  };
+};
+
+export class LocalGameProfileManager {
+  private static StorageKey = 'zwoo:gameProfiles';
+  private logger: BaseLogger;
+
+  private constructor(log: BaseLogger) {
+    this.logger = log;
+  }
+
+  private readonlyError = new Error('xy.errors.onlineOnly');
+
+  public static async create() {
+    const loggerModule = await import('@/core/services/logging/logImport');
+    return new LocalGameProfileManager(loggerModule.Logger);
+  }
+
+  public saveProfiles(content: string) {
+    localStorage.setItem(LocalGameProfileManager.StorageKey, content);
+    this.logger.info('synced local game profiles');
+  }
+
+  public getProfiles(): string {
+    return localStorage.getItem(LocalGameProfileManager.StorageKey) ?? '[]';
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public saveProfile(name: string, profile: string) {
+    throw this.readonlyError;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public updateProfile(id: string, profile: string) {
+    throw this.readonlyError;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public deleteProfile(id: string) {
+    throw this.readonlyError;
+  }
+}
