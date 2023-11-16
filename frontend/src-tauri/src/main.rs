582fad57319d28d004207ef6238d02812ad73ade
fabiankachlock <fabian.kachlock@icloud.com>
2023-11-16 21:45:50 +0100

    ZWOO-414: remove native menu & fix fullscreen

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -6,18 +6,18 @@
 use tauri::{Menu, MenuItem, Submenu};
 
 fn main() {
-    let system_menu = Submenu::new(
-        "zwoo",
-        Menu::new()
-            .add_native_item(MenuItem::Hide)
-            .add_native_item(MenuItem::Separator)
-            .add_native_item(MenuItem::CloseWindow)
-            .add_native_item(MenuItem::Quit),
-    );
-    let menu = Menu::new().add_submenu(system_menu);
+    // let system_menu = Submenu::new(
+    //     "zwoo",
+    //     Menu::new()
+    //         .add_native_item(MenuItem::Hide)
+    //         .add_native_item(MenuItem::Separator)
+    //         .add_native_item(MenuItem::CloseWindow)
+    //         .add_native_item(MenuItem::Quit),
+    // );
+    // let menu = Menu::new().add_submenu(system_menu);
 
     tauri::Builder::default()
-        .menu(menu)
+        // .menu(menu)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
