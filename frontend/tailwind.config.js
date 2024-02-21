dd8933ba419a7c508bab6bfe73a6ff8011006ce9
fabiankachlock <fabian.kachlock@icloud.com>
2024-02-21 21:55:36 +0100

    ZWOO-446 feat: add fira as font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -71,7 +71,7 @@ module.exports = {
         }
       },
       fontFamily: {
-        sans: "'sans', apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+        sans: "'Fira Sans', sans",
         mono: "'IBM Plex Mono', source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace"
       },
       animation: {
