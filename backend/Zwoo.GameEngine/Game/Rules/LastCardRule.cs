fefb74f541bab30ed2247f7333eec3e4ec5a54d4
fabiankachlock <fabian.kachlock@icloud.com>
2024-03-21 19:05:40 +0100

    fix: make code build again

--- backend/Zwoo.GameEngine/Game/Rules/LastCardRule.cs
+++ backend/Zwoo.GameEngine/Game/Rules/LastCardRule.cs
@@ -117,7 +117,7 @@ internal class LastCardRule : BaseDrawRule
         return gameEvent.Type == ClientEventType.RequestEndTurn;
     }
 
-    public override GameStateUpdate ApplyRule(ClientEvent gameEvent, GameState state, IPile cardPile, IPlayerCycle playerOrder)
+    public override GameStateUpdate ApplyRule(ClientEvent gameEvent, GameState state, Pile cardPile, PlayerCycle playerOrder)
     {
         if (!IsResponsible(gameEvent, state)) return GameStateUpdate.None(state);
         ClientEvent.RequestEndTurnEvent payload = gameEvent.CastPayload<ClientEvent.RequestEndTurnEvent>();
