efb78de006bd999c3f94af8a14a0db9c2138b8c7
fabiankachlock <fabian.kachlock@icloud.com>
2024-04-24 16:39:13 +0200

    ZWOO-464 now fixed?

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -11,7 +11,7 @@ launch_app() {
     # start env
     echo ""
     echo "starting application..."
-    docker compose -f ./e2e/docker-compose.e2e.yml up
+    docker compose -f ./e2e/docker-compose.e2e.yml up -d --wait
     echo "application started!"
 }
 
