d66c0fa16f953e68662b53ea31b29b0802a7d41b
fabiankachlock <fabian.kachlock@icloud.com>
2024-05-20 21:04:05 +0200

    fix: builds for dev instance

--- backend/Zwoo.Backend.Shared/Api/Discover/DiscoverService.cs
+++ backend/Zwoo.Backend.Shared/Api/Discover/DiscoverService.cs
@@ -13,15 +13,22 @@ public class BetaDiscoverService : IDiscoverService
 {
 
     private ZwooOptions _options;
+    private readonly string? _versionOverride;
 
     public BetaDiscoverService(ZwooOptions options)
     {
         _options = options;
+        _versionOverride = Environment.GetEnvironmentVariable("ZWOO_VERSION_OVERRIDE");
     }
 
     public bool CanConnect(ClientInfo client)
     {
         // only allow same version connects
+        if (_versionOverride != null)
+        {
+            return _versionOverride == client.Version && client.Mode == "online";
+        }
+
         return _options.App.AppVersion == client.Version && _options.App.ServerMode == client.Mode;
     }
 
