bfd4248736f5ddfeff221e1661f1067d264bfc37
fabiankachlock <fabian.kachlock@icloud.com>
2024-09-27 16:47:38 +0200

    release: v1.0.0-rc.0

--- backend/Zwoo.Backend.LocalServer/Version.cs
+++ backend/Zwoo.Backend.LocalServer/Version.cs
@@ -2,6 +2,6 @@ namespace Zwoo.Backend.LocalServer;
 
 public static class VersionProvider
 {
-    public const string VERSION = "1.0.0-beta.18";
-    public const string HASH = "cbf86843";
+    public const string VERSION = "1.0.0-rc.0";
+    public const string HASH = "bf652cc0";
 }
--- backend/Zwoo.Dashboard/Version.cs
+++ backend/Zwoo.Dashboard/Version.cs
@@ -2,6 +2,6 @@ namespace Zwoo.Dashboard;
 
 public static class VersionProvider
 {
-    public const string VERSION = "1.0.0-beta.18";
-    public const string HASH = "cbf86843";
+    public const string VERSION = "1.0.0-rc.0";
+    public const string HASH = "bf652cc0";
 }
