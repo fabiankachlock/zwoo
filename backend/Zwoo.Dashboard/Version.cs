8e7da4b736dd23b528465a60945a20957527a92d
fabiankachlock <fabian.kachlock@icloud.com>
2024-09-27 17:30:55 +0200

    release: v1.0.0-rc.1

--- backend/Zwoo.Dashboard/Version.cs
+++ backend/Zwoo.Dashboard/Version.cs
@@ -3,5 +3,5 @@ namespace Zwoo.Dashboard;
 public static class VersionProvider
 {
     public const string VERSION = "1.0.0-rc.1";
-    public const string HASH = "cad4fa3c";
+    public const string HASH = "8674c2ca";
 }
