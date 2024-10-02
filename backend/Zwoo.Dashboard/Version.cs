284d438f277ee8fe16ad139f246cb1f50dbc5772
fabiankachlock <fabian.kachlock@icloud.com>
2024-10-02 17:40:33 +0200

    ZWOO-492 fix: improve new card theme

--- backend/Zwoo.Dashboard/Version.cs
+++ backend/Zwoo.Dashboard/Version.cs
@@ -3,5 +3,5 @@ namespace Zwoo.Dashboard;
 public static class VersionProvider
 {
     public const string VERSION = "1.0.0-rc.1";
-    public const string HASH = "7fe139c5";
+    public const string HASH = "a9920c6f";
 }
