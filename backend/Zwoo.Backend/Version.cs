89622862696b1175902e4c03a4c9da7e29b84a4d
fabiankachlock <fabian.kachlock@icloud.com>
2024-10-30 21:06:32 +0100

    release: v1.0.0-rc.2

--- backend/Zwoo.Backend/Version.cs
+++ backend/Zwoo.Backend/Version.cs
@@ -2,6 +2,6 @@ namespace Zwoo.Backend;
 
 public static class VersionProvider
 {
-    public const string VERSION = "1.0.0-rc.1";
-    public const string HASH = "6cfb3a9c";
+    public const string VERSION = "1.0.0-rc.2";
+    public const string HASH = "c313d98f";
 }
